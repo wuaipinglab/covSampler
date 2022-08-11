665a3a1f59cb745884e1d96f16b6429a16cfaf56
yexiao-cheng <yxcheng2021@gmail.com>
2022-08-11 17:20:06 +0800

    Add pango lineage alias: BJ (Omicron)

--- scripts/ncov_sampling.py
+++ scripts/ncov_sampling.py
@@ -97,6 +97,7 @@ def get_target_ids(args):
                         'BF': 'Omicron',
                         'BG': 'Omicron',
                         'BH': 'Omicron',
+                        'BJ': 'Omicron',
                         'B.1.427': 'Epsilon',
                         'B.1.429': 'Epsilon',
                         'P.2': 'Zeta',
