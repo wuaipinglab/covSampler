9e7bcb2b945d761692f87b8249e1557f3478f9c8
yexiao-cheng <yxcheng2021@gmail.com>
2022-09-16 12:07:10 +0800

    Add pango lineage alias: BT (Omicron)

--- scripts/ncov_sampling.py
+++ scripts/ncov_sampling.py
@@ -106,6 +106,7 @@ def get_target_ids(args):
                         'BQ': 'Omicron',
                         'BR': 'Omicron',
                         'BS': 'Omicron',
+                        'BT': 'Omicron',
                         'B.1.427': 'Epsilon',
                         'B.1.429': 'Epsilon',
                         'P.2': 'Zeta',
