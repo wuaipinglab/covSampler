12e7cc4d7cd5e532a8641e0baf0babebe1ddaa92
yexiao-cheng <yxcheng2021@gmail.com>
2022-11-01 12:46:35 +0800

    Add pango lineage alias: CW (Omicron)

--- scripts/ncov_sampling.py
+++ scripts/ncov_sampling.py
@@ -132,6 +132,7 @@ def get_target_ids(args):
                         'CT': 'Omicron',
                         'CU': 'Omicron',
                         'CV': 'Omicron',
+                        'CW': 'Omicron',
                         'B.1.427': 'Epsilon',
                         'B.1.429': 'Epsilon',
                         'P.2': 'Zeta',
