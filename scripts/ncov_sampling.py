a51e133763739698c6bec6c8ad08bddcc48e0ade
yexiao-cheng <yxcheng2021@gmail.com>
2022-11-24 17:03:08 +0800

    Add pango lineage alias: DJ (Omicron)

--- scripts/ncov_sampling.py
+++ scripts/ncov_sampling.py
@@ -143,6 +143,7 @@ def get_target_ids(args):
                         'DF': 'Omicron',
                         'DG': 'Omicron',
                         'DH': 'Omicron',
+                        'DJ': 'Omicron',
                         'B.1.427': 'Epsilon',
                         'B.1.429': 'Epsilon',
                         'P.2': 'Zeta',
