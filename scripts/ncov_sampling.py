741043162505c91166eca4ba197e68ba99f80c3c
yexiao-cheng <yxcheng2021@gmail.com>
2023-01-02 10:41:48 +0800

    Add pango lineage alias: DQ (Omicron)

--- scripts/ncov_sampling.py
+++ scripts/ncov_sampling.py
@@ -149,6 +149,7 @@ def get_target_ids(args):
                         'DM': 'Omicron',
                         'DN': 'Omicron',
                         'DP': 'Omicron',
+                        'DQ': 'Omicron',
                         'B.1.427': 'Epsilon',
                         'B.1.429': 'Epsilon',
                         'P.2': 'Zeta',
