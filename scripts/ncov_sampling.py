1cefe26917333ed6086c1ddaf15037ba43bcfec0
yexiao-cheng <yxcheng2021@gmail.com>
2023-03-16 23:31:50 +0800

    Add pango lineage alias: ET (Omicron)

--- scripts/ncov_sampling.py
+++ scripts/ncov_sampling.py
@@ -171,6 +171,7 @@ def get_target_ids(args):
                         'EQ': 'Omicron',
                         'ER': 'Omicron',
                         'ES': 'Omicron',
+                        'ET': 'Omicron',
                         'B.1.427': 'Epsilon',
                         'B.1.429': 'Epsilon',
                         'P.2': 'Zeta',
