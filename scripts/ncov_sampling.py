47924f02801cae62f6459f596adfeb0f419ed1bf
yexiao-cheng <yxcheng2021@gmail.com>
2023-04-06 15:57:58 +0800

    Add pango lineage alias: FN (Omicron)

--- scripts/ncov_sampling.py
+++ scripts/ncov_sampling.py
@@ -183,6 +183,7 @@ def get_target_ids(args):
                         'FJ': 'Omicron',
                         'FK': 'Omicron',
                         'FM': 'Omicron',
+                        'FN': 'Omicron',
                         'B.1.427': 'Epsilon',
                         'B.1.429': 'Epsilon',
                         'P.2': 'Zeta',
